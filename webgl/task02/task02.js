44d4d0b43a3019fafa6d9ecd81328586908b8843
vincenttesla <vencent.tesla@gmail.com>
2017-02-27 17:43:36 +0800

    add ambient light

--- webgl/task02/task02.js
+++ webgl/task02/task02.js
@@ -3,8 +3,9 @@ function init(){
 	var renderer = new THREE.WebGLRenderer({
 		canvas:document.getElementById('mainCanvas')
 	})
-	renderer.setClearColor(0x777777)
+	renderer.setClearColor(0x666666)
 	renderer.shadowMap.enabled = true;
+	renderer.shadowMapSoft = true;
 
 	//scene
 	var scene = new THREE.Scene();
@@ -19,12 +20,13 @@ function init(){
 
 	//cube
 	var cube = new THREE.Mesh(new THREE.CubeGeometry(2,2,4),
-		new THREE.MeshLambertMaterial({
-			color : 0xdddddd
+		new THREE.MeshPhongMaterial({
+			color : 0xe2e2e2
 			// wireframe:true
 		})
 	);
 	cube.castShadow = true;
+	cube.receiveShadow = true;
 	scene.add(cube);
 
 	//plane
@@ -39,28 +41,28 @@ function init(){
 	//torus
 	var torus1 = new THREE.Mesh(new THREE.TorusGeometry(0.3,0.1,12,18),
 		new THREE.MeshPhongMaterial({
-			color : 0xdddddd,
+			color : 0xe2e2e2,
 			specular: 0xffffff,
 			shininess: 70
 		})
 	);
 	var torus2 = new THREE.Mesh(new THREE.TorusGeometry(0.3,0.1,12,18),
 		new THREE.MeshPhongMaterial({
-			color : 0xdddddd,
+			color : 0xe2e2e2,
 			specular: 0xffffff,
 			shininess: 70
 		})
 	);
 	var torus3 = new THREE.Mesh(new THREE.TorusGeometry(0.3,0.1,12,18),
 		new THREE.MeshPhongMaterial({
-			color : 0xdddddd,
+			color : 0xe2e2e2,
 			specular: 0xffffff,
 			shininess: 70
 		})
 	);
 	var torus4 = new THREE.Mesh(new THREE.TorusGeometry(0.3,0.1,12,18),
 		new THREE.MeshPhongMaterial({
-			color : 0xdddddd,
+			color : 0xe2e2e2,
 			specular: 0xffffff,
 			shininess: 70
 		})
@@ -77,24 +79,34 @@ function init(){
 	torus2.castShadow = true;
 	torus3.castShadow = true;
 	torus4.castShadow = true;
+	torus1.receiveShadow = true;
+	torus2.receiveShadow = true;
+	torus3.receiveShadow = true;
+	torus4.receiveShadow = true;
 	scene.add(torus1);
 	scene.add(torus2);
 	scene.add(torus3);
 	scene.add(torus4);
 
 	//light
-	var light = new THREE.DirectionalLight(0xffffff,1.3);
-	light.position.set(-4,3,-3);
+	// var light = new THREE.DirectionalLight(0xffffff,1.3);
+	var light = new THREE.SpotLight(0xffffff, 0.8, 100, Math.PI/3, 25);
+	light.position.set(-4,3,-2);
 	light.target = cube;
 	light.castShadow = true;
 
 	light.shadow.camera.near = 2;
 	light.shadow.camera.far = 10;
-	light.shadow.camera.fov = 30;
+	// light.shadow.camera.fov = 30;
+	light.shadow.camera.fov = Math.PI/3;
 
 	light.shadow.mapSize.width = 1024;
 	light.shadow.mapSize.height = 1024;
 	scene.add(light);
+
+	// ambient light
+	var ambient = new THREE.AmbientLight(0x333333);
+	scene.add(ambient);
 
 	//render
 	renderer.render(scene,camera);
